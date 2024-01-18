77ac13d36ffdd89ce6b27891dca66343559a4a40
yusufbozkurt <yusufbozkurt2022@gmail.com>
2024-01-19 00:01:50 +0300

    custom error handler and mongo db add

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -8,6 +9,8 @@ import { signoutRouter } from './routes/signout';
 
 import { errorHandler } from './middleware/error-handler';
 
+import { NotFoundError } from './errors/not-found-error';
+
 const app = express();
 app.use(json());
 
@@ -15,6 +18,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
