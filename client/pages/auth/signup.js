84ff88bb9eef46c33eedff2b237e081b9e510401
yusufbozkurt <yusufbozkurt2022@gmail.com>
2024-01-25 00:54:59 +0300

    send request from client to server

---
+++ client/pages/auth/signup.js
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import UseRequest from '../../hooks/use-request';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = UseRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <h1>Signup</h1>
+
+        <div className="form-group">
+          <label>Email Address</label>
+          <input
+            type="text"
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        {errors}
+
+        <button className="btn btn-primary">Sign Up</button>
+      </form>
+    </>
+  );
+};
+
+export default Signup;
