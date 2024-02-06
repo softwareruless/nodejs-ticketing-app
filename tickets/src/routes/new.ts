2deed6d4f3f3175ec31594e14436b13c5a89208c
yusufbozkurt <yusufbozkurt2022@gmail.com>
2024-02-06 14:17:52 +0300

    list tickets

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,32 @@
-import { requireAuth } from '@ybticketing/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@ybticketing/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (res: Response, req: Request) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than price'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+
+    await ticket.save();
+
+    res.status(201).send(ticket);
+  }
+);
 
 export { router as createTicketRouter };
---
+++ tickets/src/routes/show.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@ybticketing/common';
+import { Ticket } from '../models/ticket';
+
+const router = express.Router();
+
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  res.send(ticket);
+});
+
+export { router as showTicketRouter };
